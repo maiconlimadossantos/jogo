9420f8a8fe6129f9487779aa2e94bc2c92868fa7
maiconlimadossantos <maiconlimadossantos8@gmail.com>
2025-12-05 20:29:53 -0300

    05/12/2025

---
+++ universo.java
@@ -0,0 +1,373 @@
+public class universo {
+    public String Uniao;
+    public String Trevas;
+    public String Luz;
+    public String Fogo;
+    public String Agua;
+    public String Terra;
+    public String Ar;
+    public String Fumaca;
+    public String Lava;
+    public String Areia;
+    public String Gelo;
+    public String Tempo;
+    public String Veneno;
+    public String Energia;
+    public String Fantasma;
+    public String Hibrido;
+    public String Realidade;
+    public String Espaco;
+    public String Cosmica;
+    public String CorpoCelestial;
+    public String Lunar;
+    public String Solar;
+    public String Plasma;
+    public String Explosao;
+    public String tecnologia;
+    public String Som;
+    public String Silencio;
+    public String Quimica;
+    public String Magia;
+    public String AntiMagia;
+    public String MortoVivo;
+    public String Alquimia; 
+    public String Vidro; 
+    public String Magnética;
+    public String Eletricidade; 
+    public String Plantas;
+    public String Bolhas;
+    public String Vapor; 
+    public String Térmica; 
+    public String Calor; 
+    public String Frio;
+    public String Neve;
+    public String Gravidade; 
+    public String Mental;
+    public String Almas;
+    public String Mana;
+    public String ForcaVital;
+    public String Imaterial; 
+    public String Material ;
+    public String Irrealidade; 
+    public String Armas ;
+    public String Insetos ;
+    public String Animais ;
+    public String Genética ;
+    public String Corrupção ;
+    public String Purificação ;
+    public String Platina ;
+    public String Ouro;
+    public String Prata;
+    public String Bronze ;
+    public String Metal;
+    public String Nuclear; 
+    public String NanoRobo ;
+    public String Karma ;
+    public String Sistema;
+    public String ArtesMarciais;
+    public String Jogador;
+    public String Summer ;
+    public String PoderAntigo ;
+    public String Aco;
+    public String Relâmpago;
+    public String Sangue;
+
+   public universo(String Uniao,String Trevas,String Luz,String Fogo,String Agua,String Terra,String Ar,String Fumaca,String Lava,String Areia,String Gelo,String Tempo,String Veneno,String Energia,String Fantasma,String Hibrido,String Realidade,String Espaco,String Cosmica,String CorpoCelestial,String Lunar,String Solar,String Plasma,String Explosao,String tecnologia,String Som,String Silencio,String Quimica,String Magia,String AntiMagia,String MortoVivo,String Alquimia,String Vidro,String Magnética,String Eletricidade,String Plantas,String Bolhas,String Vapor,String Térmica,String Calor,String Frio,String Neve,String Gravidade,String Mental,String Almas ,String Mana ,String ForcaVital ,String Imaterial ,String Material ,String Irrealidade ,String Armas ,String Insetos ,String Animais ,String Genética ,String Corrupção ,String Purificação ,String Platina ,String Ouro ,String Prata ,String Bronze ,String Metal ,String Nuclear ,String NanoRobo ,String Karma ,String Sistema ,String ArtesMarciais ,String Jogador ,String Summer ,String PoderAntigo ,String Aco ,String Relâmpago ,String Sangue) {
+    this.Uniao=Uniao;
+    this.Trevas=Trevas;
+    this.Luz=Luz;
+    this.Fogo=Fogo;
+    this.Agua=Agua;
+    this.Terra=Terra;
+    this.Ar=Ar;
+    this.Fumaca=Fumaca;
+    this.Lava=Lava;
+    this.Areia=Areia;
+    this.Gelo=Gelo;
+    this.Tempo=Tempo;
+    this.Veneno=Veneno;
+    this.Energia=Energia;
+    this.Fantasma=Fantasma;
+    this.Hibrido=Hibrido;
+    this.Realidade=Realidade;
+    this.Espaco=Espaco;
+    this.Cosmica=Cosmica;
+    this.CorpoCelestial=CorpoCelestial;
+    this.Lunar=Lunar;
+    this.Solar=Solar;
+    this.Plasma=Plasma;
+    this.Explosao=Explosao;
+    this.tecnologia=tecnologia;
+    this.Som=Som;
+    this.Silencio=Silencio;
+    this.Quimica=Quimica;
+    this.Magia=Magia;
+    this.AntiMagia=AntiMagia;
+    this.MortoVivo=MortoVivo;
+    this.Alquimia=Alquimia;
+    this.Vidro=Vidro;
+    this.Magnética=Magnética;
+    this.Eletricidade=Eletricidade;
+    this.Plantas=Plantas;
+    this.Bolhas=Bolhas;
+    this.Vapor=Vapor;
+    this.Térmica=Térmica;
+    this.Calor=Calor;
+    this.Frio=Frio;
+    this.Neve=Neve;
+    this.Gravidade=Gravidade;
+    this.Mental=Mental;
+    this.Almas=Almas;
+    this.Mana=Mana;
+    this.ForcaVital=ForcaVital;
+    this.Imaterial=Imaterial;
+    this.Material=Material;
+    this.Irrealidade=Irrealidade;
+    this.Armas=Armas;
+    this.Insetos=Insetos;
+    this.Animais=Animais;
+    this.Genética=Genética;
+    this.Corrupção=Corrupção;
+    this.Purificação=Purificação;
+    this.Platina=Platina;
+    this.Ouro=Ouro;
+    this.Prata=Prata;
+    this.Bronze=Bronze;
+    this.Metal=Metal;
+    this.Nuclear=Nuclear;
+    this.NanoRobo=NanoRobo;
+    this.Karma=Karma;
+    this.Sistema=Sistema;
+    this.ArtesMarciais=ArtesMarciais;
+    this.Jogador=Jogador;
+    this.Summer=Summer;
+    this.PoderAntigo=PoderAntigo;
+    this.Aco=Aco;
+    this.Relâmpago=Relâmpago;
+    this.Sangue=Sangue;
+
+
+    }
+
+    public String getUniao() {
+        return Uniao;
+    }
+    public String getTrevas() {
+        return Trevas;
+    }
+
+    public String getLuz() {
+        return Luz;
+    }
+
+    public String getFogo() {
+        return Fogo;
+    }
+
+    public String getAgua() {
+        return Agua;
+    }
+    public String getTerra() {
+        return Terra;
+    }
+    public String getAr() {
+        return Ar;
+    }
+    public String getFumaca() {
+        return Fumaca;
+    }
+    public String getLava() {
+        return Lava;
+    }
+    public String getAreia() {
+        return Areia;
+    }
+    public String getGelo() {
+        return Gelo;
+    }
+    public String getTempo() {
+        return Tempo;
+    }
+    public String getVeneno() {
+        return Veneno;
+    }
+    public String getEnergia() {
+        return Energia;
+    }
+    public String getFantasma() {
+        return Fantasma;
+    }
+    public String getHibrido() {
+        return Hibrido;
+    }
+    public String getRealidade() {
+        return Realidade;
+    }
+    public String getEspaco() {
+        return Espaco;
+    }
+    public String getCosmica() {
+        return Cosmica;
+    }
+    public String getCorpoCelestial() {
+        return CorpoCelestial;
+    }
+    public String getLunar() {
+        return Lunar;
+    }
+    public String getSolar() {
+        return Solar;
+    }
+    public String getPlasma() {
+        return Plasma;
+    }
+    public String getExplosao() {
+        return Explosao;
+    }
+    public String gettecnologia() {
+        return tecnologia;
+    }
+    public String getSom() {
+        return Som;
+    }
+    public String getSilencio() {
+        return Silencio;
+    }
+    public String getQuimica() {
+        return Quimica;
+    }
+    public String getMagia() {
+        return Magia;
+    }
+    public String getAntiMagia() {
+        return AntiMagia;
+    }
+    public String getMortoVivo() {
+        return MortoVivo;
+    }
+    public String getAlquimia() {
+        return Alquimia;
+    }
+    public String getVidro() {
+        return Vidro;
+    }
+    public String getMagnética() {
+        return Magnética;
+    }
+    public String getEletricidade() {
+        return Eletricidade;
+    }
+    public String getPlantas() {
+        return Plantas;
+    }
+    public String getBolhas() {
+        return Bolhas;
+    }
+    public String getVapor() {
+        return Vapor;
+    }
+    public String getTérmica() {
+        return Térmica;
+    }
+    public String getCalor() {
+        return Calor;
+    }
+    public String getFrio() {
+        return Frio;
+    }
+    public String getNeve() {
+        return Neve;
+    }
+    public String getGravidade() {
+        return Gravidade;
+    }
+    public String getMental() {
+        return Mental;
+    }
+    public String getAlmas() {
+        return Almas;
+    }
+    public String getMana() {
+        return Mana;
+    }
+    public String getForcaVital() {
+        return ForcaVital;
+    }
+    public String getImaterial() {
+        return Imaterial;
+    }
+    public String getMaterial() {
+        return Material;
+    }
+    public String getIrrealidade() {
+        return Irrealidade;
+    }
+    public String getArmas() {
+        return Armas;
+    }
+    public String getInsetos() {
+        return Insetos;
+    }
+    public String getAnimais() {
+        return Animais;
+    }
+    public String getGenética() {
+        return Genética;
+    }
+    public String getCorrupção() {
+        return Corrupção;
+    }
+    public String getPurificação() {
+        return Purificação;
+    }
+    public String getPlatina() {
+        return Platina;
+    }
+    public String getOuro() {
+        return Ouro;
+    }
+    public String getPrata() {
+        return Prata;
+    }
+    public String getBronze() {
+        return Bronze;
+    }
+    public String getMetal() {
+        return Metal;
+    }
+    public String getNuclear() {
+        return Nuclear;
+    }
+    public String getNanoRobo() {
+        return NanoRobo;
+    }
+    public String getKarma() {
+        return Karma;
+    }
+    public String getSistema() {
+        return Sistema;
+    }
+    public String getArtesMarciais() {
+        return ArtesMarciais;
+    }
+    public String getJogador() {
+        return Jogador;
+    }
+    public String getSummer() {
+        return Summer;
+    }
+    public String getPoderAntigo() {
+        return PoderAntigo;
+    }
+    public String getAco() {
+        return Aco;
+    }
+    public String getRelâmpago() {
+        return Relâmpago;
+    }
+    public String getSangue() {
+        return Sangue;
+    }
+
+
+}
---
+++ universos.java
@@ -0,0 +1,3 @@
+public class universos extends universo  {
+    
+}
